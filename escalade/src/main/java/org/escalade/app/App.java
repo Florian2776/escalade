8074caf8effcfbd1b2ac86c34c7d77048a8e078b
Florian2776 <florianbrun271994@gmail.com>
2017-12-06 18:03:29 +0100

    rajout de la section build dans pom.xmlprincipale, création d'un jar exécutable

--- escalade/src/main/java/org/escalade/app/App.java
+++ escalade/src/main/java/org/escalade/app/App.java
@@ -4,10 +4,8 @@ package org.escalade.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "\nBienvenue sur l'application Montagne et escalade !\n" );
     }
 }
