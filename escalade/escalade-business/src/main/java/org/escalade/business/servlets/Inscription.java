c9130ecae95b01c18bb6c9f03ad9cec63f9ac974
Florian2776 <florianbrun271994@gmail.com>
2018-01-19 17:45:55 +0100

    mise en place de la partie business

---
+++ escalade/escalade-business/src/main/java/org/escalade/business/servlets/Inscription.java
@@ -0,0 +1,63 @@
+package org.escalade.business.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.escalade.model.beans.Utilisateur;
+import org.escalade.consumer.dao.*;
+
+/**
+ * Inscription implementation class Inscription
+ */
+@WebServlet("/Inscription")
+public class Inscription extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private UtilisateurDao utilisateurDao;
+
+    public void init() throws ServletException {
+        DaoFactory daoFactory = DaoFactory.getInstance();
+        this.utilisateurDao = daoFactory.getUtilisateurDao();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            request.setAttribute("utilisateurs", utilisateurDao.lister());
+        }
+        catch (DaoException e) {
+            request.setAttribute("erreur", e.getMessage());
+        }
+        this.getServletContext().getRequestDispatcher("/WEB-INF/inscription.jsp").forward(request, response);
+    }
+
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        try {
+            Utilisateur utilisateur = new Utilisateur();
+//            utilisateur.setPseudo(request.getParameter("pseudo"));
+//            utilisateur.setNom(request.getParameter("nom"));
+//            utilisateur.setPrenom(request.getParameter("prenom"));
+//            if (request.getParameter("mdp").compareTo(request.getParameter("mdp2")) != 0) {
+//                throw new ServletException("Les mots de passes ne sont pas identiques");
+//            }
+//            utilisateur.setPassword(request.getParameter("mdp"));
+//            utilisateur.setExperience(request.getParameter("exp"));
+//            utilisateur.setEmail(request.getParameter("email"));
+//            utilisateur.setTel(request.getParameter("tel"));
+
+            utilisateurDao.ajouter(utilisateur);
+            request.setAttribute("utilisateurs", utilisateurDao.lister());
+        }
+        catch (Exception e) {
+            request.setAttribute("erreur", e.getMessage());
+        }
+        
+        this.getServletContext().getRequestDispatcher("/WEB-INF/inscription.jsp").forward(request, response);
+    }
+    
+    
+
+}
---
+++ escalade/escalade-business/src/main/java/org/escalade/business/servlets/Servlet.java
@@ -0,0 +1,43 @@
+package org.escalade.business.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.escalade.model.beans.Utilisateur;
+import org.escalade.consumer.dao.*;
+
+/**
+ * Servlet implementation class Servlet
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private UtilisateurDao utilisateurDao;
+
+    public void init() throws ServletException {
+        DaoFactory daoFactory = DaoFactory.getInstance();
+        this.utilisateurDao = daoFactory.getUtilisateurDao();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	try {
+            request.setAttribute("utilisateurs", utilisateurDao.lister());
+        }
+        catch (DaoException e) {
+            request.setAttribute("erreur", e.getMessage());
+        }
+        this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+    }
+
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+    }
+    
+    
+
+}
